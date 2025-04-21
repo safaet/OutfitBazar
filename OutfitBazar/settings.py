0e14c543a5d14c92b22f46b6e3eed796f3d637d9
safaet <safaetjaman@gmail.com>
2025-04-21 20:25:19 +0600

    simple login and signin

--- OutfitBazar/settings.py
+++ OutfitBazar/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'OutfitBazar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'  # optional
--- store/views.py
+++ store/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+# from .forms import CustomUserCreationForm
 
 def home(request):
     products = Product.objects.all()
@@ -8,3 +13,61 @@ def home(request):
 def product_detail(request, pk):
     product = Product.objects.get(pk=pk)
     return render(request, 'store/product_detail.html', {'product': product})
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Redirect to login page after signup
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+# def registerUser(request):
+#     page = 'register'
+#     form = CustomUserCreationForm()
+
+#     if request.method == 'POST':
+#         form = CustomUserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save(commit=False)
+#             user.username = user.username.lower()
+#             user.save()
+
+#             messages.success(request, 'User was created successfully')
+
+#             login(request, user)
+#             return redirect('profiles')
+#         else:
+#             messages.error(request, 'An error has occurred during registration')
+            
+
+#     context = {'page': page, 'form': form}
+#     return render(request, 'users/login_register.html', context)
+
+def loginUser(request):
+
+    if request.user.is_authenticated:
+        return redirect('profiles')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Username does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request, 'Username OR password is incorrect') 
+
+    return render(request, 'registration/login.html')
